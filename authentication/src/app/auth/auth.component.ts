9a26ce670af8762cc541022f67470da81083fc15
gudik-zoe <tony_khoury993@hotmail.com>
2020-05-17 20:51:25 +0200

    handeling errors

--- authentication/src/app/auth/auth.component.ts
+++ authentication/src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 import { HttpClient} from '@angular/common/http';
 import { ɵangular_packages_platform_browser_platform_browser_d } from '@angular/platform-browser';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
@@ -21,16 +22,18 @@ myForm:FormGroup
   switch(){
     this.signIn = !this.signIn
   }
-  email = this.myForm.get('email').value
- password = this.myForm.get('password').value
+
+
+
   signUp(){   
+    let email = this.myForm.get('email').value
+ let password = this.myForm.get('password').value
     this.isloading = true
     if (!this.signIn){
-    
     this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1dSw4fhE_vyBAYYCeuafUIdDAH-cP_9c',
     {
-      email: this.email , 
-      password: this.password,
+      email: email , 
+      password: password,
       returnSecureToken : true
     }
     ).subscribe(data => {
@@ -42,17 +45,46 @@ myForm:FormGroup
       if(error.error.error.message == 'EMAIL_EXISTS'){
       this.errorMessage ='the mail already exists'
       }
+
+      else {
+        this.error = true 
+        this.errorMessage = 'unknown error occured'
+      }
     })
   }
   
   else{
-    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1dSw4fhE_vyBAYYCeuafUIdDAH-cP_9c',
+    this.isloading = true
+    this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA1dSw4fhE_vyBAYYCeuafUIdDAH-cP_9c',
     {
-      email:this.email,
-      password:this.password,
-      
-    })
+      email:email,
+      password:password,
+      returnSecureToken : true
+
+    }).subscribe(
+      data => {
+        
+        console.log('succeded' , data)
+        this.isloading = false
+      },
+      error =>{
+        this.isloading = false
+        this.error = true
+        if (error.error.error.message == 'EMAIL_NOT_FOUND'){
+            this.errorMessage = 'email not found'
+        }
+        else if 
+          (error.error.error.message == 'INVALID_PASSWORD'){
+            this.errorMessage = 'incorrect password'
+        }
+        else {
+        this.errorMessage = 'dono what happened'
+       
+        }
+      }
+    )
   }
+  this.myForm.reset()
 }
     ngOnInit() {
  this.myForm = this.fb.group({
