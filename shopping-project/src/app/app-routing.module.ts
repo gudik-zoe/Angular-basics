f2a27aa2dcaed2b1cf5040672dc54c2dc3084050
tony khoury <tony_khoury993@hotmail.com>
2020-05-25 19:06:16 +0200

    more products

--- shopping-project/src/app/app-routing.module.ts
+++ shopping-project/src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
 import { RegistrationComponent } from './registration/registration.component';
 
-
 const routes: Routes = [
-  {path:'products' , component:ProductsComponent},
-  {path:'card' , component:CartComponent},
-  {path:'purchase' , component:RegistrationComponent}
+  { path: '', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'card', component: CartComponent },
+  { path: 'purchase', component: RegistrationComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
