fdba3f72d5dddc211058260a71b749e30f3439f8
tony khoury <tony_khoury993@hotmail.com>
2020-05-25 21:03:35 +0200

    nav edited

--- shopping-project/src/app/app.component.ts
+++ shopping-project/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartServiceService } from './cart-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private router: Router, private service: CartServiceService) {}
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+  products = [];
   title = 'shopping-project';
+
+  goToCard() {
+    this.router.navigate(['/card']);
+  }
+
+  ngOnInit() {
+    this.products = this.service.getCardItems();
+  }
 }
