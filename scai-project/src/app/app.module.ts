c18a1b68466362af0c642c6aded6afb2a944bd78
gudik-zoe <tony_khoury993@hotmail.com>
2020-06-08 12:52:48 +0200

    registration form (no error msgs)

--- scai-project/src/app/app.module.ts
+++ scai-project/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    
     BrowserModule,
     AppRoutingModule
   ],
---
+++ scai-project/src/app/log-in/log-in.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent implements OnInit {
+  constructor(private fb: FormBuilder ) { }
+
+  submit(){
+    console.log(this.signUpForm.value)
+  }
+signUpForm:FormGroup
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      name: ['' , Validators.required],
+      lastName:['' , Validators.required],
+      email:['' , Validators.required],
+      password: ['' , Validators.required],
+      confirmPassword:['' , Validators.required],
+      gender:['', Validators.required],
+    })
+    
+  }
+
+}
